dd2f9865d421b7e080359b687f73724053c3403d
Samandar <samandarwebdeveloper@gmail.com>
2024-03-26 13:23:34 +0500

    add babel js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,59 @@
-const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, 'src/js/main.js'),
-        about: path.resolve(__dirname, 'src/js/about.js'),
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "src/js/main.js"),
+    about: path.resolve(__dirname, "src/js/about.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Countries | Home",
+      template: "./src/indexTemp.html",
+      filename: "index.html",
+      chunks: ["main"],
+    }),
+    new htmlWebpackPlugin({
+      title: "Countries | About",
+      template: "./src/pages/aboutTemp.html",
+      filename: "about.html",
+      chunks: ["about"],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name][contenthash].js',
-        clean: true
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            title: "Countries | Home",
-            template: "./src/indexTemp.html",
-            filename: "index.html",
-            chunks: ['main']
-        }),
-        new htmlWebpackPlugin({
-            title: "Countries | About",
-            template: "./src/pages/aboutTemp.html",
-            filename: "about.html",
-            chunks: ['about']
-        }),
-        new MiniCssExtractPlugin()
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, "css-loader"],
-          },
-        ],
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-}
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
