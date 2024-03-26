9b5de74f4ac20dfe7602476463354942bcf30f71
Samandar <samandarwebdeveloper@gmail.com>
2024-03-26 12:33:04 +0500

    about section

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,15 @@
 import '../css/main.css'
+import './mode'
+import request from './request'
+import { createCountryInfo } from './updateUI'
+
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const country = urlParams.get('country')
+const countryApi = `https://restcountries.com/v3.1/${country}`
+
+request(countryApi).then((data) => {
+    createCountryInfo(data[0])
+}).catch((error) => {
+    console.log(error)
+})
--- src/js/updateUI.js
+++ src/js/updateUI.js
@@ -1,7 +1,7 @@
-const ul = document.querySelector('.cards')
+const ul = document.querySelector(".cards");
 
 export const createCountries = (countries) => {
-  ul.innerHTML = ''
+  ul.innerHTML = "";
   countries.forEach((country) => {
     const commonName = country.name.common;
     const population = country.population;
@@ -12,7 +12,7 @@ export const createCountries = (countries) => {
     const li = document.createElement("li");
     li.classList.add("cards__item");
     li.innerHTML = `
-        <a href="./about.html">
+        <a href="./about.html?country=/name/${commonName}">
               <img
                 src=${flag}
                 alt="usa-flag"
@@ -27,7 +27,86 @@ export const createCountries = (countries) => {
               </div>
             </a> 
         `;
-      ul.appendChild(li)
+    ul.appendChild(li);
   });
 };
 
+const countryInfoEl = document.querySelector(".country-info");
+export const createCountryInfo = (country) => {
+  const {
+    population,
+    flags,
+    borders,
+    name,
+    languages,
+    capital,
+    tld,
+    subregion,
+    currencies,
+    region,
+  } = country;
+
+  const nativeName = Object.values(name.nativeName)[0].official;
+  const currency = Object.values(currencies)[0].name;
+  const language = Object.values(languages);
+
+  countryInfoEl.innerHTML = `
+    
+    <img class="country-info__img" src=${
+      flags.svg
+    } alt="germany-flag" width="560" height="400">
+    <div class="country-info__content">
+      <h2>${name.common}</h2>
+      <ul class="country-info__list">
+        <li class="country-info__item">
+          <p class="name">
+            Native Name:
+            <span>${nativeName}</span>
+          </p>
+          <p class="population">
+            Population:
+            <span>${population}</span>
+          </p>
+          <p class="region">
+            Region:
+            <span>${region}</span>
+          </p>
+          <p class="sub-region">
+            Sub Region:
+            <span>${subregion}</span>
+          </p>
+          <p class="capital">
+            Capital:
+            <span>${capital}</span>
+          </p>
+        </li>
+        <li class="country-info__item">
+          <p class="name">
+            Top Level Domain:
+            <span>${tld}</span>
+          </p>
+          <p class="population">
+            Currencies:
+            <span>${currency}</span>
+          </p>
+          <p class="region">
+            Languages:
+            <span>${language}</span>
+          </p>
+        </li>
+      </ul>
+
+      <div class="country-info__borders">
+        <h3>Border Countries:</h3>
+        ${
+          borders
+            ? borders.map((border) => {
+                return `<a href="./about.html?country=/alpha/${border}">${border}</a>
+            `;
+              })
+            : "No borders"
+        }        
+      </div>
+    </div>
+  `;
+};
